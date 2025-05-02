61d1df80c1f621bbde4e37ae1b2d1bf10dfaeb87
XU Ao <xuao575@gmail.com>
2025-05-02 13:18:09 +0800

    Initial Commit

---
+++ HStackCardsTest/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  HStackCardsTest
+//
+//  Created by Ao XU on 5/2/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ HStackCardsTest/HStackCardsTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HStackCardsTestApp.swift
+//  HStackCardsTest
+//
+//  Created by Ao XU on 5/2/25.
+//
+
+import SwiftUI
+
+@main
+struct HStackCardsTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
